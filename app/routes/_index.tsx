8e4a549f2fb3402135e9f9f2757f548d5e0ab3c8
Michael Craig <michael@michaelcraig.group>
2023-10-29 12:43:26 -0700

    logo updates

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -66,75 +66,29 @@ export default function Index() {
           </div>
         </div>
 
-        <div className="mx-auto max-w-7xl px-4 py-2 sm:px-6 lg:px-8">
-          <div className="mt-6 flex flex-wrap justify-center gap-8">
-            {[
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157991167-651c8fc5-2f72-4afa-94d8-2520ecbc5ebc.svg",
-                alt: "AWS",
-                href: "https://aws.com",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157991935-26c0d587-b866-49f5-af34-8f04be1c9df2.svg",
-                alt: "DynamoDB",
-                href: "https://aws.amazon.com/dynamodb/",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157990874-31f015c3-2af7-4669-9d61-519e5ecfdea6.svg",
-                alt: "Architect",
-                href: "https://arc.codes",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a-0ac7b65c8c14.svg",
-                alt: "Tailwind",
-                href: "https://tailwindcss.com",
-              },
-              {
-                src: "",
-                alt: "Playwright",
-                href: "https://playwright.dev/",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772386-75444196-0604-4340-af28-53b236faa182.svg",
-                alt: "MSW",
-                href: "https://mswjs.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772447-00fccdce-9d12-46a3-8bb4-fac612cdc949.svg",
-                alt: "Vitest",
-                href: "https://vitest.dev",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772662-92b0dd3a-453f-4d18-b8be-9fa6efde52cf.png",
-                alt: "Testing Library",
-                href: "https://testing-library.com",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772934-ce0a943d-e9d0-40f8-97f3-f464c0811643.svg",
-                alt: "Prettier",
-                href: "https://prettier.io",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157772990-3968ff7c-b551-4c55-a25c-046a32709a8e.svg",
-                alt: "ESLint",
-                href: "https://eslint.org",
-              },
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157773063-20a0ed64-b9f8-4e0b-9d1e-0b65a3d4a6db.svg",
-                alt: "TypeScript",
-                href: "https://typescriptlang.org",
-              },
-            ].map((img) => (
-              <a
-                key={img.href}
-                href={img.href}
-                className="flex h-16 w-32 justify-center p-1 grayscale transition hover:grayscale-0 focus:grayscale-0"
-              >
-                <img alt={img.alt} src={img.src} className="object-contain" />
-              </a>
-            ))}
-          </div>
-        </div>
+        <div className="mx-auto mt-8 max-w-7xl px-4 py-2 sm:px-6 lg:px-8">
+					<div className="flex flex-wrap justify-center gap-8 rounded-3xl bg-slate-100 py-4 dark:bg-slate-200">
+						<TooltipProvider>
+							{logos.map(img => (
+								<Tooltip key={img.href}>
+									<TooltipTrigger asChild>
+										<a
+											href={img.href}
+											className="flex h-16 w-32 justify-center p-1 grayscale transition hover:grayscale-0 focus:grayscale-0"
+										>
+											<img
+												alt={img.alt}
+												src={img.src}
+												className="object-contain"
+											/>
+										</a>
+									</TooltipTrigger>
+									<TooltipContent>{img.alt}</TooltipContent>
+								</Tooltip>
+							))}
+						</TooltipProvider>
+					</div>
+				</div>
       </div>
     </main>
   );
---
+++ app/routes/logos/logos.ts
@@ -0,0 +1,95 @@
+import architect from './architect.svg'
+import eslint from './eslint.svg'
+import fakerJS from './faker.svg'
+import github from './github.svg'
+import msw from './msw.svg'
+import playwright from './playwright.svg'
+import prettier from './prettier.svg'
+import dynamoDB from './dynamoDB.svg'
+import remix from './remix.png'
+import shadcnUI from './shadcn-ui.svg'
+import tailwind from './tailwind.svg'
+import testingLibrary from './testing-library.png'
+import typescript from './typescript.svg'
+import vitest from './vitest.svg'
+import zod from './zod.svg'
+
+export { default as stars } from './stars.jpg'
+
+export const logos = [
+	{
+		src: remix,
+		alt: 'Remix',
+		href: 'https://remix.run',
+	},
+	{
+		src: architect,
+		alt: 'Architect',
+		href: 'https://arc.codes/',
+	},
+	{
+		src: dynamoDB,
+		alt: 'DynamoDB',
+		href: 'https://aws.amazon.com/dynamodb/',
+	},
+	{
+		src: zod,
+		alt: 'Zod',
+		href: 'https://zod.dev/',
+	},
+	{
+		src: github,
+		alt: 'GitHub',
+		href: 'https://github.com',
+	},
+	{
+		src: tailwind,
+		alt: 'Tailwind',
+		href: 'https://tailwindcss.com',
+	},
+	{
+		src: shadcnUI,
+		alt: 'shadcn/ui',
+		href: 'https://ui.shadcn.com/',
+	},
+	{
+		src: playwright,
+		alt: 'Playwright',
+		href: 'https://playwright.dev/',
+	},
+	{
+		src: msw,
+		alt: 'MSW',
+		href: 'https://mswjs.io',
+	},
+	{
+		src: fakerJS,
+		alt: 'Faker.js',
+		href: 'https://fakerjs.dev/',
+	},
+	{
+		src: vitest,
+		alt: 'Vitest',
+		href: 'https://vitest.dev',
+	},
+	{
+		src: testingLibrary,
+		alt: 'Testing Library',
+		href: 'https://testing-library.com',
+	},
+	{
+		src: typescript,
+		alt: 'TypeScript',
+		href: 'https://typescriptlang.org',
+	},
+	{
+		src: prettier,
+		alt: 'Prettier',
+		href: 'https://prettier.io',
+	},
+	{
+		src: eslint,
+		alt: 'ESLint',
+		href: 'https://eslint.org',
+	},
+]
